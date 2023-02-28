d8ae7674e840ae029400ae91f9656876693941d1
Mads Bjørk Ohmsen <mads80m3@stud.kea.dk>
2023-02-28 11:18:11 +0100

    Character Dto's

---
+++ CSharp_Assignment_3_MovieApi/Models/Dto/CharacterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSharp_Assignment_3_MovieApi.Models.Dto
+{
+    public class CharacterDto
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Alias { get; set; }
+
+        public string Gender { get; set; }
+     
+        public string Picture { get; set; }
+
+        public List<string> Movies { get; set; }
+    }
+}
